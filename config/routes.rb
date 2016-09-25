99cf8db09fb33db785714f9e0c40af5f0d4668fe
m-toma-cubic <m.toma.cubic@gmail.com>
2016-09-25 14:28:08 +0900

    投稿編集・削除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  get 'notes/new'
-
-  get '/top' => 'home#top'
-  get '/about' => 'home#about'
-  get '/notes/:id' => 'notes#show', as:'note'
-
+  get '/notes/new' => 'notes#new'
   post '/notes' => 'notes#create'
+  get '/notes' => 'notes#index'
+  get '/notes/:id' => "notes#show", as: 'note'
+  get  '/notes/:id/edit' =>"notes#edit", as: 'edit_note'
+  patch  '/notes/:id' =>"notes#update", as: 'update_note'
+  delete '/notes/:id' =>"notes#destroy", as:'destroy_note'
 
   root 'home#top'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/about' => 'home#about'
 end
