0bbe738a3f1a9b0c05afa30826895bde433b67b6
m-toma-cubic <m.toma.cubic@gmail.com>
2016-09-27 08:27:14 +0900

    ユーザモデルに画像カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160925140759) do
+ActiveRecord::Schema.define(version: 20160926232512) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160925140759) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
 end
